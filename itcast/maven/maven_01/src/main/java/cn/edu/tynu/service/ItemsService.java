158b0369aa05ef0b3f7876a4a921bf7aad89d785
k0y0c <yuchang_kan@163.com>
2022-01-16 20:50:53 +0800

    使用maven的web demo

---
+++ itcast/maven/maven_01/src/main/java/cn/edu/tynu/service/ItemsService.java
@@ -0,0 +1,7 @@
+package cn.edu.tynu.service;
+
+import cn.edu.tynu.domain.Items;
+
+public interface ItemsService {
+    public Items findById(Integer id);
+}
---
+++ itcast/maven/maven_parent/maven_dao/src/main/java/cn/edu/tynu/dao/ItemsDao.java
@@ -0,0 +1,7 @@
+package cn.edu.tynu.dao;
+
+import cn.edu.tynu.domain.Items;
+
+public interface ItemsDao {
+    public Items findById(Integer id);
+}
